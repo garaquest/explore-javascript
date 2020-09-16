f6c0d8a61679ce9c8b00adc3d0161383149e2c3d
garaquest <garaquest@gmail.com>
2020-09-17 00:06:17 +0700

    Assign variable

--- 02/variable.js
+++ 02/variable.js
@@ -4,3 +4,6 @@ let numbers = [1,2,3,4,5]
 console.log(num == numbers)
 
 // An array, which is a non-primitive data type is mutable. Non-primitive data type cannot be compared by value. Even if two non-primitive data types have the same properties and values, they are not strictly equal.
+
+num = numbers
+console.log(num == numbers)
