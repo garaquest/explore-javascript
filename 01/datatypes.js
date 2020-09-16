7d73ccb8a8b4468c14c45914ebc555509946fb1f
garaquest <garaquest@gmail.com>
2020-09-16 23:55:23 +0700

    Datatypes

---
+++ 01/datatypes.js
@@ -0,0 +1,23 @@
+// Numbers
+let number = 100
+
+// Strings
+let name = "Satoshi Nakamoto"
+
+// Booleans
+let yes = true
+let no = false
+
+// undefined
+let unknown
+
+// null
+let something = null
+
+// Checking datatypes
+console.log(typeof number)
+console.log(typeof name)
+console.log(typeof yes)
+console.log(typeof no)
+console.log(typeof unknown)
+console.log(typeof something)
