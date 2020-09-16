132b5acf0e19e6d320d7055ecb4f96024c9ef993
garaquest <garaquest@gmail.com>
2020-09-17 00:00:32 +0700

    Non-primitive datatypes

---
+++ 02/variable.js
@@ -0,0 +1,6 @@
+let num = [1,2,3,4,5]
+let numbers = [1,2,3,4,5]
+
+console.log(num == numbers)
+
+// An array, which is a non-primitive data type is mutable. Non-primitive data type cannot be compared by value. Even if two non-primitive data types have the same properties and values, they are not strictly equal.
