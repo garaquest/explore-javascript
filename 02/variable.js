03e47fbce12ddecaa13569563c8063d74e1b52d7
garaquest <garaquest@gmail.com>
2020-09-17 01:49:05 +0700

    Ends With

--- 02/variable.js
+++ 02/variable.js
@@ -75,3 +75,4 @@ console.log(book.lastIndexOf("Germ"))
 let founder = "Vitalik Buterin"
 console.log(founder.concat(" is"," Ethereum's"," Founder."))
 console.log(founder.startsWith("V"))
+console.log(founder.endsWith("n"))
